6d4393f2e331ab89f54b66d2948750f61c76c188
nataliya.pasichnyk <nataliya.pasichnyk@codility.com>
2020-11-08 19:11:06 +0100

    Add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,20 @@ import React from 'react';
 import './App.css';
 
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import CoursesList from "./Courses";
+import CoursesList from './Courses';
 
 const client = new ApolloClient({
   uri: 'https://platform-me.herokuapp.com/',
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 });
 
 function App() {
-
   return (
-      <ApolloProvider client={client}>
-    <div className="App">
-    <CoursesList/>
-    </div></ApolloProvider>
-
+    <ApolloProvider client={client}>
+      <div className="App">
+        <CoursesList />
+      </div>
+    </ApolloProvider>
   );
 }
 
--- src/Courses.tsx
+++ src/Courses.tsx
@@ -1,24 +1,21 @@
-import {useQuery} from '@apollo/client';
 import React from 'react';
-import { Course } from './generated/graphql';
+import { useQuery } from '@apollo/client';
 import { loader } from 'graphql.macro';
-
+import { Course } from './generated/graphql';
 
 const CoursesList = () => {
-	const query = loader('./graphql/courses.graphql');
-	const { loading, error, data } = useQuery(query);
+  const query = loader('./graphql/courses.graphql');
+  const { loading, error, data } = useQuery(query);
 
-	if (loading) return <p>Loading...</p>;
-	if (error) return <p>Error :(</p>;
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
 
-
-	return ( data.courses.map((course: Course, id: number)=> (
-		<div key={id}>
-			<p>{course.title}</p>
-			<p>{course.author}</p>
-		</div>
-	))
-	);
+  return data.courses.map((course: Course) => (
+    <div key={course.title}>
+      <p>{course.title}</p>
+      <p>{course.author}</p>
+    </div>
+  ));
 };
 
 export default CoursesList;
